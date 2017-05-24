6ec54d8c8ac84c47fb43e8c23654cd1654a2039c
williammian <william_mian@yahoo.com.br>
2017-05-24 16:42:05 -0300

    spring forum

---
+++ src/main/java/br/com/springforum/conf/ServletSpringMVC.java
@@ -0,0 +1,25 @@
+package br.com.springforum.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer{
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[]{AppWebConfiguration.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[]{};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+	
+	
+
+}
